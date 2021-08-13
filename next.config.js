ccb3861b5bcdbe44c996d554decf14fe4b69f78f
Ankur Paul <suryashi2013@gmail.com>
2021-08-13 03:57:42 +0000

    Updates for NextJS 11.1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  // Prefer loading of ES Modules over CommonJS
+  experimental: { esmExternals: true },
+  // https://nextjs.org/blog/next-11-1#builds--data-fetching
+  /* httpAgentOptions: {
+    keepAlive: false,
+  }, */
+};
