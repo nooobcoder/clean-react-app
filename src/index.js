7d4636f5c2b5ff076fa1811e219618447cfc495f
nooobcoder <suryashi2013@gmail.com>
2022-03-10 20:12:38 +0530

    Major change conforming to React 18 release: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.htm

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createRoot} from "react-dom/client";
 
+// ⚠️ React 17 code (Soon to be LEGACY!) ⚠️
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
+
+// ⚠️ NEW! React 18 has the new Client Rendering API.
+// Read more from the release candidate: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App/>)
